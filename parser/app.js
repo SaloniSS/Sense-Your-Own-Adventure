b01842b2fcc8fe8508848572f6baaf9d81643dbe
Ishpreet Singh Bhasin <ishpreet2000@gmail.com>
2019-04-06 18:57:53 -0500

    Project Structure

---
+++ parser/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const connectWithRetry = () => {
+	return mongoose.connect('mongodb://mongo:27017/rooms', { useNewUrlParser: true }, (err) => {
+		if (err) {
+			console.error('\nFailed to connect to mongo on startup - retrying in 5 sec\n', err, '\n');
+			setTimeout(connectWithRetry, 5000);
+		} else {
+			console.error('Connected to mongo\n');
+
+
+
+		}
+	});
+};
+connectWithRetry();
+
+module.exports = app;
