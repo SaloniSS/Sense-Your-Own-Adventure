cb013c94e5887ea3fda4db8fb1dc7511b670980d
Ishpreet Singh Bhasin <ishpreet2000@gmail.com>
2019-04-06 21:35:39 -0500

    AwakeBars

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import rnl2b from 'react-newline-to-break';
 import './App.scss'
 
 class App extends Component {
+
+
 	render() {
+		let text = "You are stranded on what looks like a lonely island. It is pretty sunny outside. You look around but no ones around you. From a distance, you notice a trail that walks to the forest.\nDo you want to walk to the forest or walk along the shore?";
+		text = rnl2b(text);
+
 		return (
 			<div className="message-box">
-				<p>
-					You are stranded on what looks like a lonely island. It is pretty sunny outside.
-					You look around but no ones around you.
-					From a distance, you notice a trail that walks to the forest.
-				</p>
-				<p>
-					Do you want to walk to the forest or walk along the <b>shore</b>?
-				</p>
-
+				<div className="message-text">
+					<p>
+						{ text }
+					</p>
+				</div>
 				<input type="text" id="choice" placeholder="Your Choice.." />
 			</div>
 		);
