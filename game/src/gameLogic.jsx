11d4dc7cb698b22d1f879ade08b3dc4f4045b3b7
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-04-06 22:42:50 -0500

    Update gameLogic.jsx
    ish is on my back smh

--- game/src/gameLogic.jsx
+++ game/src/gameLogic.jsx
@@ -6,7 +6,8 @@ export class GameText extends React.Component {
         super(props);
         this.state = {
             currNode : props.wholeData,
-            input : ''
+            input : '',
+            optMessage:''
         };
     }
 
@@ -15,22 +16,31 @@ export class GameText extends React.Component {
     };
 
     checkInput = () => {
-        console.log(this.state.currNode.right.data.name);
+        this.setState({
+            optMessage: ''
+        });
         if (this.state.input === this.state.currNode.right.data.name) {
             this.setState({
                 currNode: this.state.currNode.right
             });
-            this.nextPart();
-        } else alert("not whoa");
-        console.log("changed: " + this.state.currNode.right.data.name);
+        }
+        else if (this.state.input === this.state.currNode.left.data.name) {
+            this.setState({
+                currNode: this.state.currNode.left
+            });
+        }
+        else {
+            this.setState( {
+                optMessage: "Input not recognized. Please enter either " + this.state.currNode.right.data.name + " or " + this.state.currNode.left.data.name
+            })
+        }
+        this.setState({
+            input: ''
+        });
         //Else if >50% close to one word say “Did you mean to type (child keyword)? (Keyword) is spelled (). Please retype.”
         //Add incorrect Node’s data to an array
         //Else say “Input not recognized. Please enter either (list child keywords).”
     };
-
-    nextPart() {
-
-    }
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -39,21 +49,39 @@ export class GameText extends React.Component {
 
 
     render() {
-        let text = this.state.currNode.data.story;
-        text = rnl2b(text);
-
-        return (
-            <div className="message-box">
-                <div className="message-text">
-                    <p>
-                        { text }
-                    </p>
+        if (this.state.currNode.right === undefined) {
+            return (
+                <div className="message-box">
+                    <div className="message-text">
+                        <p className="upper">
+                            Thank you for playing!
+                        </p>
+                    </div>
                 </div>
-                <form className="form" onSubmit={ this.handleSubmit }>
-                    <input type="text" className="input" id="choice" onChange={this.handleChange.bind(this)} />
-                    <input type="submit" className="submit" value="Enter" />
-                </form>
-            </div>
-        );
+            );
+         }
+        else {
+            let text = this.state.currNode.data.story;
+            text = rnl2b(text);
+
+            return (
+                <div className="message-box">
+                    <div className="message-text">
+                        <p className="upper">
+                            {text}
+                        </p>
+                        <p className="lower">
+                            Type <span className="keyWord">{this.state.currNode.right.data.name}</span> or <span
+                            className="keyWord">{this.state.currNode.left.data.name}</span>
+                        </p>
+                        <p className="optMessage">{this.state.optMessage}</p>
+                    </div>
+                    <form className="form" onSubmit={this.handleSubmit}>
+                        <input type="text" className="input" id="choice" onChange={this.handleChange.bind(this)}/>
+                        <input type="submit" className="submit" value="Enter"/>
+                    </form>
+                </div>
+            );
+        }
     }
 }
