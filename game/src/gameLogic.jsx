8b5c76a0ec3d0764b2cdf8d0afc4c2bf8267e96e
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-04-06 21:54:51 -0500

    Update gameLogic.jsx
    more game logic

--- game/src/gameLogic.jsx
+++ game/src/gameLogic.jsx
@@ -5,20 +5,34 @@ export class GameText extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            currNode : props.wholeData
+            currNode : props.wholeData,
+            input : ''
         };
     }
 
     handleChange = (event) => {
-        this.setState({storyText: event.target.value});
-    }
+        this.setState({input: event.target.value});
+    };
 
     handleKeyPress = (event) => {       // stop getting user input on enter
         if(event.key === 'Enter'){
-            // checkInput();
-            alert(this.state.currNode.data.name);
+            this.checkInput();
         }
-    }
+    };
+
+    checkInput = () => {
+        /*
+        Compare input (after pressing enter) to both child’s keywords
+        If the input matches one of the child node’s, set Node equal to the right child node
+        Call nextPart(Node)
+        Else if >50% close to one word say “Did you mean to type (child keyword)? (Keyword) is spelled (). Please retype.”
+        Add incorrect Node’s data to an array
+        Else say “Input not recognized. Please enter either (list child keywords).”
+         */
+        if(this.state.input == this.state.currNode.right.data.name) {
+            alert("whoa");
+        } else alert("not whoa");
+    };
 
     nextPart(Node) {
         this.setState({storyText: Node.target.value});
@@ -35,7 +49,7 @@ export class GameText extends React.Component {
                         { text }
                     </p>
                 </div>
-                <input type="text" id="choice" onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)}/>
+                <input type="text" class="input" id="choice" onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)}/>
             </div>
         );
     }
