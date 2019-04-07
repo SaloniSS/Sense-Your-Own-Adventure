a4be8fea6c828f4adcc5e52ef7bfc797f17b320d
Ishpreet Singh Bhasin <ishpreet2000@gmail.com>
2019-04-07 06:05:23 -0500

    Code Don e

--- game/src/gameLogic.jsx
+++ game/src/gameLogic.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import rnl2b from 'react-newline-to-break';
 import Speech from 'speak-tts';
+import Sound from 'react-sound';
 
 export class GameText extends React.Component {
     speech = new Speech();
@@ -9,17 +10,23 @@ export class GameText extends React.Component {
         super(props);
         this.state = {
             currNode : {
-            	data: {}
+            	data: {
+            		sound: "undefined.wav"
+	            },
+	            left: props.wholeData,
+	            right: props.wholeData
             },
-            input : '',
-            optMessage:''
+            input : 'Begin',
+            optMessage:'',
+	        sound: Sound.status.STOPPED,
+	        lastSpoken: ''
         };
-        this.textToSpeechInit(() => {
-	        this.changeNode(props.wholeData); // cb is the call back function passed
-        });
+        this.textToSpeechInit();
+	    this.handleSubmit = this.handleSubmit.bind(this);
+	    this.handleChange = this.handleChange.bind(this);
     }
 
-    textToSpeechInit(cb) {
+    textToSpeechInit(cb = () => {}) {
         this.speech.init({
             'volume': 1,
             'lang': 'en-GB',
@@ -28,51 +35,138 @@ export class GameText extends React.Component {
             'voice':'Google UK English Male',
             'splitSentences': true
         }).then((data) => {
-            // The "data" object contains the list of available voices and the voice synthesis params
-            console.log("Speech is ready, voices are available", data)
 	        cb();
-        }).catch(e => {
-            console.error("An error occured while initializing : ", e)
         });
     }
 
 
     changeNode(elem) {
-        this.setState({
-            currNode: elem
-        });
+    	if (elem.data.sound === undefined || elem.data.sound === "") {
+    		elem.data.sound = "undefined.wav";
+	    }
+	    this.setState({
+		    currNode: elem,
+		    input: ''
+	    }, () => {
+		    this.speech.cancel();
+	    	this.setState({
+			    sound: Sound.status.PLAYING
+		    });
+	    });
     }
 
-    handleChange = (event) => {
-        this.setState({input: event.target.value});
+    handlePlaying = (soundObj) => {
+    	if (soundObj.position >= 5000) {
+			this.handleFinishPlaying();
+	    }
+    };
+
+    handleFinishPlaying = () => {
+	    this.setState({
+		    sound:  Sound.status.STOPPED
+	    }, () => {
+		    this.speak();
+	    });
+    };
+
+    speak = () => {
+	    const elem = this.state.currNode;
+	    if (elem.data.story) {
+		    let text = elem.data.story;
+		    if (elem.left !== undefined && elem.right !== undefined) {
+			    text += 'Type ' + elem.right.data.name + ' or ' + elem.left.data.name;
+		    }
+		    if (text !== this.state.lastSpoken) {
+		    	this.setState({
+				    lastSpoken: text
+			    });
+			    this.speech.speak({
+				    text: text
+			    });
+		    }
+	    }
+    };
+
+    handleChange(event) {
+	    if (this.speech.speaking() || this.state.sound === Sound.status.PLAYING) {
+		    this.setState({input: ''});
+	    } else {
+		    this.setState({input: event.target.value});
+	    }
     };
 
     checkInput = () => {
         this.setState({
             optMessage: ''
+        }, () => {
+        	const input = (this.state.input).toLowerCase().replace(/[^a-z]+/g, '');
+	        const left = (this.state.currNode.left.data.name).toLowerCase().replace(/[^a-z]+/g, '');
+	        const right = (this.state.currNode.right.data.name).toLowerCase().replace(/[^a-z]+/g, '');
+	        if (input === right) {
+	            this.changeNode(this.state.currNode.right);
+	        }
+	        else if (input === left) {
+	            this.changeNode(this.state.currNode.left);
+	        }
+	        else {
+	            this.setState( {
+	                optMessage: "Input not recognized. Please enter either " + this.state.currNode.right.data.name + " or " + this.state.currNode.left.data.name
+	            }, () => {
+	                const elem = this.state.currNode;
+		            const text = 'Type ' + elem.right.data.name + ' or ' + elem.left.data.name;
+					this.speech.speak({
+						text: text
+					});
+	            });
+	        }
+	        this.setState({
+	            input: ''
+	        });
         });
-        if (this.state.input === this.state.currNode.right.data.name) {
-            this.changeNode(this.state.currNode.right);
-        }
-        else if (this.state.input === this.state.currNode.left.data.name) {
-            this.changeNode(this.state.currNode.left);
-        }
-        else {
-            // this.speak("Input not recognized. Please enter either " + this.state.currNode.right.data.name + " or " + this.state.currNode.left.data.name);
-            this.setState( {
-                optMessage: "Input not recognized. Please enter either " + this.state.currNode.right.data.name + " or " + this.state.currNode.left.data.name
-            })
-        }
-        this.setState({
-            input: ''
-        });
-        //Else if >50% close to one word say “Did you mean to type (child keyword)? (Keyword) is spelled (). Please retype.”
-        //Add incorrect Node’s data to an array
     };
 
-    handleSubmit = (event) => {
+    handleSubmit(event) {
         event.preventDefault();
-        this.checkInput();
+	    if (this.speech.speaking() || this.state.sound === Sound.status.PLAYING) {
+	    } else {
+	        this.checkInput();
+        }
+    };
+
+    lowerMessage = (
+		<span>
+		</span>
+	);
+    form = (
+		<span>
+		</span>
+	);
+
+    calculateText = () => {
+	    if ((this.state.currNode.right !== undefined) && (this.state.currNode.left !== undefined)) {
+		    this.lowerMessage = (
+			    <span>
+				    Type <span className="keyWord">{this.state.currNode.right.data.name}</span> or <span
+				    className="keyWord">{this.state.currNode.left.data.name}</span>
+			    </span>
+		    );
+		    this.form = (
+			    <form className="form" onSubmit={this.handleSubmit}>
+				    <input type="text" className="input" id="choice" autoFocus
+				           onChange={ this.handleChange } value={ this.state.input } tabIndex={1} />
+				    <input type="submit" className="submit" value="Enter" tabIndex={-1} />
+			    </form>
+		    );
+	    } else {
+		    this.lowerMessage = (
+			    <span>
+			    </span>
+		    );
+		    this.form = (
+			    <span>
+			    </span>
+		    );
+	    }
     };
 
 
@@ -80,44 +174,23 @@ export class GameText extends React.Component {
 	    let text = this.state.currNode.data.story;
 	    text = rnl2b(text);
 
-        let lowerMessage = (
-            <div>
-            </div>
-        );
-        let form = (
-            <div>
-            </div>
-        );
-        if ((this.state.currNode.right !== undefined) && (this.state.currNode.left !== undefined)) {
-	        lowerMessage = (
-		        <div>
-			        Type <span className="keyWord">{this.state.currNode.right.data.name}</span> or <span
-			        className="keyWord">{this.state.currNode.left.data.name}</span>
-		        </div>
-	        );
-	        form = (
-		        <form className="form" onSubmit={ this.handleSubmit }>
-			        <input type="text" className="input" id="choice" autoFocus onChange={this.handleChange.bind(this)} />
-			        <input type="submit" className="submit" value="Enter" />
-		        </form>
-	        );
-        }
-
+	    this.calculateText();
 
         return (
             <div className="container">
-                <img className="image" src={process.env.PUBLIC_URL + "/images/" + this.state.currNode.data.image} alt={this.state.currNode.data.image} />
+	            <Sound url={process.env.PUBLIC_URL + "/sounds/" + this.state.currNode.data.sound} playStatus={ this.state.sound } autoLoad onFinishedPlaying={ this.handleFinishPlaying } onStop={ this.handleFinishPlaying } onError={ this.handleFinishPlaying } onPlaying={ this.handlePlaying } />
+	            <img className="image" src={process.env.PUBLIC_URL + "/images/" + this.state.currNode.data.image} alt={this.state.currNode.data.image} />
                 <div className="message-box">
                     <div className="message-text">
                         <p className="upper">
                             { text }
                         </p>
                         <p className="lower">
-                            { lowerMessage }
+                            { this.lowerMessage }
                         </p>
                         <p className="optMessage">{this.state.optMessage}</p>
                     </div>
-                    { form }
+                    { this.form }
                 </div>
             </div>
         );
